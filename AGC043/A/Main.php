451cc34776e7165804e20bcc9387875bee40b795
Hiroaki Sone <koutetsu666@gmail.com>
2020-03-22 18:27:12 +0900

    AGC043 A
    rewrite by using DP

--- AGC043/A/Main.php
+++ AGC043/A/Main.php
@@ -8,13 +8,24 @@ for ($i = 0; $i < $h; $i++) {
     $map[$i] =  preg_split('//', trim(fgets(STDIN)), -1, PREG_SPLIT_NO_EMPTY);
 }
 
-print (ceil(move(0, 0, $map[0][0] == "#" ? 1 : 0) / 2));
+print (ceil(score($h - 1, $w - 1) / 2));
 
-function move($r, $c, $count) {
+function score($r, $c) {
     global $h, $w, $map, $dp;
-    if ($r == $h -1 && $c == $w - 1) return $count;
-    $p1 = $p2 = PHP_INT_MAX;
-    if ($c < $w - 1) $p1 = move($r, $c+1, $map[$r][$c] == $map[$r][$c+1] ? $count : $count+1);
-    if ($r < $h - 1) $p2 = move($r+1, $c, $map[$r][$c] == $map[$r+1][$c] ? $count : $count+1);
-    return min($p1, $p2);
+    if (isset($dp[$r][$c])) return $dp[$r][$c];
+
+    if ($r == 0 && $c == 0) return ($map[$r][$c] == "#") ? 1 : 0;
+
+    $s1 = $s2 = PHP_INT_MAX;
+    if ($r != 0) {
+        $s1 = score($r-1, $c);
+        if ($map[$r-1][$c] != $map[$r][$c]) $s1++;
+    }
+    
+    if ($c != 0) {
+        $s2 = score($r, $c-1);
+        if ($map[$r][$c-1] != $map[$r][$c]) $s2++;
+    }
+
+    return $dp[$r][$c] = min($s1, $s2);
 }
