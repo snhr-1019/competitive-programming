0b763fc0279ca81d5ceabfc2401af84d14cc134a
Hiroaki Sone <koutetsu666@gmail.com>
2020-04-19 17:02:34 +0900

    C Problems

--- ABC155/C/src/Main.java
+++ ABC155/C/src/Main.java
@@ -1,34 +1,34 @@
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+    import java.util.*;
+    import java.util.stream.Collectors;
+    import java.util.stream.IntStream;
 
-class Main {
-    final Scanner sc = new Scanner(System.in);
-    Map<String, Integer> map = new LinkedHashMap<>();
-    public static void main(String[] args) {
-        new Main().run();
-    }
+    class Main {
+        final Scanner sc = new Scanner(System.in);
+        Map<String, Integer> map = new LinkedHashMap<>();
+        public static void main(String[] args) {
+            new Main().run();
+        }
 
-    private void run() {
-        int n = sc.nextInt();
-        IntStream.range(0, n).forEach(i -> {
-            String str = sc.next();
-            Integer val = map.get(str);
-            if (val == null) {
-                map.put(str, 1);
-            } else {
-                map.put(str, val.intValue()+1);
+        private void run() {
+            int n = sc.nextInt();
+            IntStream.range(0, n).forEach(i -> {
+                String str = sc.next();
+                Integer val = map.get(str);
+                if (val == null) {
+                    map.put(str, 1);
+                } else {
+                    map.put(str, val.intValue()+1);
+                }
+            });
+            int max = 0;
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                max = Math.max(entry.getValue(), max);
             }
-        });
-        int max = 0;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            max = Math.max(entry.getValue(), max);
-        }
 
-        Set<String> set = new TreeSet<>();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == max) set.add(entry.getKey());
+            Set<String> set = new TreeSet<>();
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == max) set.add(entry.getKey());
+            }
+            set.forEach(System.out::println);
         }
-        set.forEach(System.out::println);
     }
-}
