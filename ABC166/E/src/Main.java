741804d29e7924c5e9f3abf270442d2c71f72967
Hiroaki Sone <koutetsu666@gmail.com>
2020-05-03 22:57:39 +0900

    ABC166 E fix

--- ABC166/E/src/Main.java
+++ ABC166/E/src/Main.java
@@ -10,21 +10,17 @@ class Main {
     private void run() {
         int n = sc.nextInt();
         Map<Long, Long> add = new TreeMap<>();
-        Map<Long, Long> sub = new TreeMap<>();
         long[] arr = new long[n+1];
 
         for (int i = 1; i <= n; i++) {
             long a = sc.nextLong();
             arr[i] = a;
             add.put(i+a, add.getOrDefault(i + a, 0L)+1);
-            sub.put(i-a, sub.getOrDefault(i - a, 0L)+1);
         }
 
-        int ans = 0;
-        for(Map.Entry<Long, Long> addentry : add.entrySet()) {
-            if (sub.containsKey(addentry.getKey())) {
-                ans += addentry.getValue() * sub.get(addentry.getKey());
-            }
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans += add.getOrDefault(i-arr[i], 0L);
         }
         System.out.println(ans);
     }
