dc949b4a3b1a5b2fb16d1e541aec5d6c5b3011a6
Hiroaki Sone <koutetsu666@gmail.com>
2020-05-31 17:36:05 +0900

    NOMURA2020 C

--- nomura2020/c/Main.java
+++ nomura2020/c/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Main {
     final Scanner sc = new Scanner(System.in);
@@ -8,7 +8,38 @@ class Main {
     }
 
     private void run() {
+        int n = sc.nextInt();
+        int[] a = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            a[i] = sc.nextInt();
+        }
 
+        // 葉ではない頂点の最大数
+        long[] max = new long[n + 1];
+        max[0] = 1 - a[0];
+        if (max[0] < 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        long ln = 100000000000000L;
+        for (int i = 1; i <= n; i++) {
+            max[i] = Math.min(max[i - 1] * 2 - a[i], ln);
+            if (max[i] < 0) {
+                System.out.println(-1);
+                return;
+            }
+        }
+
+        long ans = a[n];
+        long[] b = new long[n + 1];
+        b[n] = a[n];
+        for (int i = n - 1; i >= 0; i--) {
+            b[i] = Math.min(max[i], b[i + 1]) + a[i];
+            ans += b[i];
+        }
+
+        System.out.println(ans);
     }
 }
 
