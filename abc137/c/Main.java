8a0d4a21d0a23b1fbd764735e94f10cd2143bf25
Hiroaki Sone <koutetsu666@gmail.com>
2020-06-06 15:35:14 +0900

    ABC137 C

--- abc137/c/Main.java
+++ abc137/c/Main.java
@@ -9,8 +9,25 @@ class Main {
 
     private void run() {
         int n = sc.nextInt();
-        
+        String[] s = new String[n];
 
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        for (String s1 : s) {
+            char[] c = s1.toCharArray();
+            Arrays.sort(c);
+            String sorted = String.valueOf(c);
+            map.put(sorted, map.getOrDefault(sorted, 0) + 1);
+        }
+
+        int ans = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            int t = entry.getValue();
+            ans += t * (t - 1) / 2;
+        }
+        System.out.println(ans);
     }
 }
 
