6538efc13ffd9b384496274544f2afc26323f6d3
Hiroaki Sone <koutetsu666@gmail.com>
2020-06-13 23:34:10 +0900

    TOKIOMARINE2020

---
+++ tokiomarine2020/c/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import static java.lang.Math.*;
+
+class Main {
+    final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    private void run() {
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                
+            }
+        }
+    }
+}
