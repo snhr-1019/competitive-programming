bc8f90f3d2ba333292f64886e107b3b5e438d07b
Hiroaki Sone <koutetsu666@gmail.com>
2020-07-05 23:34:05 +0900

    ABC173 C fix

--- abc173/c/Main.java
+++ abc173/c/Main.java
@@ -8,50 +8,31 @@ class Main {
         new Main().run();
     }
 
-    int h;
-    int w;
-    int k;
-    String[][] grid;
-    int[] cntH;
-    int[] cntW;
-    int cnt;
     private void run() {
-        h = sc.nextInt();
-        w = sc.nextInt();
-        k = sc.nextInt();
-        grid = new String[h][w];
-        cnt = 0;
-        cntH = new int[h];
-        cntW = new int[w];
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        int k = sc.nextInt();
+        String[][] grid = new String[h][w];
 
         for (int i = 0; i < h; i++) {
             grid[i] = sc.next().split("");
-            for (int j = 0; j < w; j++) {
-                if (grid[i][j].equals("#")) {
-                    cntH[i]++;
-                    cnt++;
-                }
-            }
-        }
-
-        for (int i = 0; i < w; i++) {
-            for (int j = 0; j < h; j++) {
-                if (grid[j][i].equals("#")) {
-                    cntW[i]++;
-                }
-            }
         }
 
         // 全探索
-        for (int i = 0; i < 1 << h; i++) {
-            for (int l = 0; l < 1 << w; l++) {
-                int tmpCnt = cnt;
-                for (int j = 0; j < h; j++) {
-                    if ((1 & i >> j) == 1) {
-                        tmpCnt -= cntW[j];
+        int ans = 0;
+        for (int rows = 0; rows < 1 << h; rows++) {
+            for (int cols = 0; cols < 1 << w; cols++) {
+                int black = 0;
+                for (int i = 0; i < h; i++) {
+                    if ((1 & rows >> i) == 1) continue;
+                    for (int j = 0; j < w; j++) {
+                        if ((1 & cols >> j) == 1) continue;
+                        if (grid[i][j].equals("#")) black++;
                     }
                 }
+                if (black == k) ans++;
             }
         }
+        System.out.println(ans);
     }
 }
