b5b4f6e86098ce992112bf4cd532028e382a39e4
Hiroaki Sone <koutetsu666@gmail.com>
2020-08-03 00:24:10 +0900

    fix ABC174 C

--- abc174/c/main.py
+++ abc174/c/main.py
@@ -1,16 +1,16 @@
 k = int(input())
 
-i = -1
-num = 0
-while True:
-    if k % 2 == 0:
-        print(-1)
-        exit(0)
-    i += 1
-    num += (7*10**i)
-    num %= k
-
+num = 7
+cnt = 1
+for i in range(k):
     if num % k == 0:
-        print(i+1)
-        exit(0)
+        break
+
+    cnt += 1
+    num = (num * 10 + 7) % k
+
+if num % k == 0:
+    print(cnt)
+else:
+    print(-1)
 
