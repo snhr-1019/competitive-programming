8903b55d26bdf3e0d0074dfb735e52b7fbf85f9a
Hiroaki Sone <koutetsu666@gmail.com>
2021-01-02 22:56:06 +0900

    ABC187 D fix

--- abc187/d/main.py
+++ abc187/d/main.py
@@ -1,7 +1,7 @@
 N = int(input())
-p = [list(map(int, input().split())) for i in range(N)]
 
-p = sorted(p, reverse=True, key=lambda x:(x[0]+x[1]))
+p = [list(map(int, input().split())) for i in range(N)]
+p.sort(reverse=True, key=lambda x: (x[0] * 2 + x[1]))
 
 sum_a = 0
 for i in range(N):
@@ -12,5 +12,5 @@ for i in range(N):
     sum_a -= p[i][0]
     sum_b += p[i][0] + p[i][1]
     if sum_b > sum_a:
-        print(i+1)
+        print(i + 1)
         exit(0)
