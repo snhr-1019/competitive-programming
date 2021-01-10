0782fd7a2cd7924360b20642a3cc07a756575a20
Hiroaki Sone <koutetsu666@gmail.com>
2021-01-10 23:00:33 +0900

    ABC188 D fix

--- abc188/d/main.py
+++ abc188/d/main.py
@@ -1,24 +1,22 @@
-from collections import defaultdict
-
 N, C = map(int, input().split())
 
-x = defaultdict(list)
-min_day = 10 ** 9 + 1
-max_day = 0
+event = []
+
 for i in range(N):
     ai, bi, ci = map(int, input().split())
-    min_day = min(min_day, ai)
-    max_day = max(max_day, bi)
-    x[ai].append(ci)
-    x[bi + 1].append(-1 * ci)
+    bi += 1
+    event.append((ai, ci))
+    event.append((bi, -ci))
 
+event.sort()
+
+sum_cost = 0
 day_cost = 0
-cost_sum = 0
-days = [0] + sorted(x.keys())
-for i in range(1, len(days)):
-    cost_sum += min(day_cost, C) * (days[i] - days[i - 1])
+t = 0
+for x, y in event:
+    if x != t:
+        sum_cost += min(day_cost, C) * (x - t)
+        t = x
+    day_cost += y
 
-    for cost in x[days[i]]:
-        day_cost += cost
-
-print(cost_sum)
+print(sum_cost)
