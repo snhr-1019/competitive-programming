cc3fe2772669441368da6382c21ca7579dc1f6dd
Hiroaki Sone <koutetsu666@gmail.com>
2021-08-09 00:06:04 +0900

    ABC213 fix

--- AtCoder/abc213/c/main.py
+++ AtCoder/abc213/c/main.py
@@ -1,28 +1,29 @@
 H, W, N = map(int, input().split())
 
-h_cards = [1] * H
-w_cards = [1] * W
-
 A = []
 B = []
 for i in range(N):
     a, b = map(int, input().split())
-    a -= 1
-    b -= 1
-    h_cards[a] = 0
-    w_cards[b] = 0
-    A.append(a)
-    B.append(b)
+    A.append([a, i])
+    B.append([b, i])
 
-h_sum = [0] * H
-h_sum[0] = h_cards[0]
-for i in range(1, H):
-    h_sum[i] = h_sum[i - 1] + h_cards[i]
+A.sort()
+B.sort()
 
-w_sum = [0] * W
-w_sum[0] = w_cards[0]
-for i in range(1, W):
-    w_sum[i] = w_sum[i - 1] + w_cards[i]
+a = 1
+b = 1
+A[0].append(a)
+B[0].append(b)
+for i in range(1, N):
+    if A[i - 1][0] != A[i][0]:
+        a += 1
+    if B[i - 1][0] != B[i][0]:
+        b += 1
+    A[i].append(a)
+    B[i].append(b)
+
+A.sort(key=lambda x: x[1])
+B.sort(key=lambda x: x[1])
 
 for i in range(N):
-    print(' '.join(map(str, [A[i] - h_sum[A[i]] + 1, B[i] - w_sum[B[i]] + 1])))
+    print(str(A[i][2]) + ' ' + str(B[i][2]))
--- AtCoder/abc213/d/main.py
+++ AtCoder/abc213/d/main.py
@@ -1,1 +1,29 @@
+import sys
 
+sys.setrecursionlimit(300000)
+
+N = int(input())
+
+e = [[] for i in range(N + 1)]
+
+for i in range(N - 1):
+    A, B = map(int, input().split())
+    e[A].append(B)
+    e[B].append(A)
+
+for i in range(N + 1):
+    e[i].sort()
+
+ans = []
+
+
+def dfs(curr, pre):
+    ans.append(curr)
+    for nxt in e[curr]:
+        if nxt != pre:
+            dfs(nxt, curr)
+            ans.append(curr)
+
+
+dfs(1, -1)
+print(*ans)
