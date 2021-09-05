b9909d613ac010bd799150645f9208e26900f080
Hiroaki Sone <koutetsu666@gmail.com>
2021-09-05 10:53:40 +0900

    ABC217 D fix

--- AtCoder/abc217/d/Main.java
+++ AtCoder/abc217/d/Main.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         int L = sc.nextInt();
         int Q = sc.nextInt();
 
-        Set<Integer> set = new TreeSet<>();
+        TreeSet<Integer> set = new TreeSet<>();
         set.add(0);
         set.add(L);
 
@@ -16,9 +16,7 @@ class Main {
             if (c == 1) {
                 set.add(x);
             } else {
-                Object[] arr = set.toArray();
-                int pos = Arrays.binarySearch(arr, x);
-                System.out.print(Integer.getInteger(arr[pos].toString()) - Integer.getInteger(arr[pos - 1].toString()));
+                System.out.println(set.higher(x) - set.lower(x));
             }
         }
     }
