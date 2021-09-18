64b7aa79d701db0469afd58cb4f14fafc3ea1c54
Hiroaki Sone <koutetsu666@gmail.com>
2021-09-18 23:03:56 +0900

    zone2021 D

--- AtCoder/zone2021/d/main.py
+++ AtCoder/zone2021/d/main.py
@@ -1,1 +1,25 @@
+T = list(input())
 
+ans = list()
+reverse = False
+for c in T:
+    if c == 'R':
+        reverse = not reverse
+        continue
+
+    if not reverse:
+        if len(ans) == 0 or ans[-1] != c:
+            ans.append(c)
+        else:
+            ans.remove(-1)
+            del ans[-1]
+    else:
+        if len(ans) == 0 or ans[0] != c:
+            ans.insert(0, c)
+        else:
+            del ans[0]
+
+if not reverse:
+    print(''.join(ans))
+else:
+    print(''.join(reversed(ans)))
