a74fda57e293c65d0b069a945c21b505492b62dd
Hiroaki Sone <koutetsu666@gmail.com>
2021-10-18 00:16:08 +0900

    ABC223 C

--- AtCoder/abc223/c/main.py
+++ AtCoder/abc223/c/main.py
@@ -1,25 +1,19 @@
 N = int(input())
 A = []
 B = []
-T = []
+t = 0
 for i in range(N):
     a, b = map(int, input().split())
     A.append(a)
     B.append(b)
-    T.append(a / b)
-
-left = 0
-right = sum(T)
+    t += a / b
+t /= 2
+ans = 0
 for i in range(N):
-    left += T[i]
-    right -= T[i]
-    # 左の所要時間が大きくなったら、そのときの導火線のどこかでぶつかるはず
-    if left >= right:
-        left -= T[i]
-        right += T[i]
-
-        # ここから2分探索
-        l = 0
-        r = A[i]
-        x = l + r / 2
-        left + (x / B[i]) - right + 
+    if t > A[i] / B[i]:
+        ans += A[i]
+        t -= A[i] / B[i]
+    else:
+        ans += B[i] * t
+        break
+print(ans)
