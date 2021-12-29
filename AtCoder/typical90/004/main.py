6407084030dd88956992c92c2bb8bef12d8ccbf8
Hiroaki Sone <koutetsu666@gmail.com>
2021-12-30 01:51:41 +0900

    典型90問 004

--- AtCoder/typical90/004/main.py
+++ AtCoder/typical90/004/main.py
@@ -1,1 +1,16 @@
+H, W = map(int, input().split())
+A = []
+horizontal_sum = [0] * H
+vertical_sum = [0] * W
 
+for i in range(H):
+    A.append(list(map(int, input().split())))
+    horizontal_sum[i] = sum(A[i])
+    for j in range(W):
+        vertical_sum[j] += A[i][j]
+
+for i in range(H):
+    ans = []
+    for j in range(W):
+        ans.append(horizontal_sum[i] + vertical_sum[j] - A[i][j])
+    print(*ans)
