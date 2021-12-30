8d41ad869475b8454cbdb752899296839b824f9c
Hiroaki Sone <koutetsu666@gmail.com>
2021-12-31 00:11:36 +0900

    典型90問 014, 016, 020

--- AtCoder/typical90/016/main.py
+++ AtCoder/typical90/016/main.py
@@ -1,1 +1,13 @@
-
+N = int(input())
+A, B, C = map(int, input().split())
+ans = 10 ** 10
+for na in range(9999):
+    if A * na > N:
+        break
+    for nb in range(9999):
+        if A * na + B * nb > N:
+            break
+        if (N - (A * na + B * nb)) % C == 0:
+            nc = (N - (A * na + B * nb)) // C
+            ans = min(ans, na + nb + nc)
+print(ans)
