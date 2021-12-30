93fea59b96e7aeb997dd6fc434909efb8551e084
Hiroaki Sone <koutetsu666@gmail.com>
2021-12-30 12:54:15 +0900

    典型90問 007, 010

--- AtCoder/typical90/007/main.py
+++ AtCoder/typical90/007/main.py
@@ -1,1 +1,10 @@
+import bisect
 
+N = int(input())
+A = [- 10 ** 10] + sorted(list(map(int, input().split()))) + [10 ** 10]
+Q = int(input())
+ans = []
+for i in range(Q):
+    B = int(input())
+    r = bisect.bisect_right(A, B)
+    print(min(B - A[r - 1], A[r] - B))
--- AtCoder/typical90/010/main.py
+++ AtCoder/typical90/010/main.py
@@ -1,1 +1,16 @@
+N = int(input())
+accum = [[0] * (N + 1) for _ in range(2)]
 
+for i in range(1, N + 1):
+    C, P = map(int, input().split())
+    C -= 1
+    accum[C][i] += accum[C][i - 1] + P
+    another = not C
+    accum[another][i] += accum[another][i - 1]
+
+Q = int(input())
+for i in range(Q):
+    L, R = map(int, input().split())
+    L -= 1
+    R -= 1
+    print(accum[0][R + 1] - accum[0][L], accum[1][R + 1] - accum[1][L])
