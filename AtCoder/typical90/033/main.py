08858144fc2fd2e7d30f17482cf73cbcde840257
Hiroaki Sone <koutetsu666@gmail.com>
2022-01-01 17:52:02 +0900

    典型90問 033, 034

--- AtCoder/typical90/033/main.py
+++ AtCoder/typical90/033/main.py
@@ -1,1 +1,7 @@
+import math
 
+H, W = map(int, input().split())
+if H == 1 or W == 1:
+    print(H * W)
+else:
+    print(math.ceil(H / 2) * math.ceil(W / 2) // 1)
--- AtCoder/typical90/034/main.py
+++ AtCoder/typical90/034/main.py
@@ -1,1 +1,22 @@
+from collections import defaultdict
 
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+types = defaultdict(int)
+cnt = 0
+ans = 0
+r = 0
+for l in range(n):
+    while r < n:
+        if types[a[r]] == 0 and cnt == k:
+            break
+        if types[a[r]] == 0:
+            cnt += 1
+        types[a[r]] += 1
+        r += 1
+    ans = max(ans, r - l)
+    if types[a[l]] == 1:
+        cnt -= 1
+    types[a[l]] -= 1
+print(ans)
