e426129d768724ad38099e485ba47cdf9da84d89
Hiroaki Sone <koutetsu666@gmail.com>
2022-01-05 20:44:56 +0900

    典型90問

--- AtCoder/typical90/048/main.py
+++ AtCoder/typical90/048/main.py
@@ -1,1 +1,11 @@
+N, K = map(int, input().split())
 
+a = [0] * N
+b = [0] * N
+q = []
+ans = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    q.append(b)
+    q.append(a - b)
+print(sum(sorted(q, reverse=True)[0:K]))
