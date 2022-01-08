ab1ef3b6947ac3343ef422bfabcb47c85cc5d72e
Hiroaki Sone <koutetsu666@gmail.com>
2022-01-09 00:11:10 +0900

    ABC234 E fix

--- AtCoder/abc234/e/main.py
+++ AtCoder/abc234/e/main.py
@@ -1,66 +1,18 @@
-x = list(map(int, input()))
+x = int(input())
 
-l = len(x)
-if l == 1 or l == 2:
-    print(''.join(map(str, x)))
-    exit()
-
-# 最初から等差数かどうか
-ok = True
-d = x[1] - x[0]
-for i in range(2, l):
-    if x[i] - x[i - 1] != d:
-        ok = False
-if ok:
-    print(''.join(map(str, x)))
-    exit()
-
-# 2桁目が2桁目と同じか小さい場合、下がっていく等差数の可能性がある
-if x[0] >= x[1]:
-    d = min(x[0] // (len(x) - 1), x[1] - x[0])
-
-    while d <= 9:
-        print(d)
-        candidate = x[:]
-        ok = True
-        for i in range(1, len(x)):
-            tmp = candidate[i - 1] + d
-            if tmp > 9 or tmp < 0 or tmp < x[i]:
-                ok = False
-                break
-            candidate[i] = tmp
-
-        if ok:
-            print(''.join(map(str, candidate)))
-            exit()
-        d += 1
-
-# 一番上の桁を上げずに等差数を作れるか
-ok = True
-d = max(0, x[1] - x[0])
-
-candidate = x[:]
-for i in range(1, l):
-    tmp = candidate[i - 1] + d
-    candidate[i] = tmp
-    if tmp > 9 or tmp < 0:
-        ok = False
-        break
-
-if ok:
-    print(''.join(map(str, candidate)))
-    exit()
-
-# 一番上の桁を上げる場合
-if x[0] == 9:
-    x = [1] + x
-    x[1] = 0
-else:
-    x[0] += 1
-
-d = x[0] // (len(x) - 1)
-candidate = x[:]
-
-for i in range(1, len(x)):
-    candidate[i] = candidate[i - 1] - d
-print(''.join(map(str, candidate)))
+for l in range(len(str(x)), 19):
+    for first in range(1, 10):
+        for d in range(-9, 9):
+            ok = True
+            a = []
+            for i in range(l):
+                tmp = first + d * i
+                a.append(tmp)
+                if tmp > 9 or tmp < 0:
+                    ok = False
+                    break
+            if ok:
+                candidate = int(''.join(map(str, a)))
+                if x <= candidate:
+                    print(candidate)
+                    exit()
