80e70312ac4633a7ac4fe18448b1712fa3657838
Hiroaki Sone <koutetsu666@gmail.com>
2022-01-09 12:23:21 +0900

    典型90問

--- AtCoder/typical90/061/main.py
+++ AtCoder/typical90/061/main.py
@@ -1,1 +1,13 @@
+from collections import deque
+
+q = int(input())
+que = deque()
+for i in range(q):
+    t, x = map(int, input().split())
+    if t == 1:
+        que.appendleft(x)
+    elif t == 2:
+        que.append(x)
+    else:
+        print(que[x - 1])
 
--- AtCoder/typical90/063/main.py
+++ AtCoder/typical90/063/main.py
@@ -1,1 +1,24 @@
+h, w = map(int, input().split())
+p = []
 
+for i in range(h):
+    p.append(list(map(int, input().split())))
+
+ans = 0
+for t in range(2 ** h):
+    i_list = []
+    for s in range(h):
+        if (t >> s) & 1:
+            i_list.append(s)
+    cnt = [0] * ((h + 1) * (w + 1))
+
+    for j in range(w):
+        ok = True
+        for i in i_list:
+            if p[i_list[0]][j] != p[i][j]:
+                ok = False
+                break
+        if len(i_list) > 0 and ok:
+            cnt[p[i_list[0]][j]] += 1
+    ans = max(ans, max(cnt) * len(i_list))
+print(ans)
