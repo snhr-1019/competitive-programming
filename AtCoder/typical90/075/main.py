4a59e016d9df70da3f878abcbefa1d9bf88a4032
Hiroaki Sone <koutetsu666@gmail.com>
2022-01-12 00:33:58 +0900

    典型90問

--- AtCoder/typical90/075/main.py
+++ AtCoder/typical90/075/main.py
@@ -1,1 +1,14 @@
+from math import log2, ceil
 
+n = int(input())
+
+div = 0
+i = 2
+while i * i <= n:
+    while n % i == 0:
+        n //= i
+        div += 1
+    i += 1
+if n != 1:
+    div += 1
+print(int(ceil(log2(div))))
