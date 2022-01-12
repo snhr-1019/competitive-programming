5c92d7058e0e727f40db57ffe9451e478703ec22
Hiroaki Sone <koutetsu666@gmail.com>
2022-01-13 01:18:53 +0900

    典型90問

--- AtCoder/typical90/076/main.py
+++ AtCoder/typical90/076/main.py
@@ -1,1 +1,19 @@
+n = int(input())
+a = list(map(int, input().split()))
+K = sum(a) / 10
 
+s = a[0]
+j = 0
+for i in range(n):
+    while True:
+        if s == K:
+            print("Yes")
+            exit()
+        elif s > K:
+            break
+
+        j = (j + 1) % n
+        s += a[j]
+    s -= a[i]
+
+print("No")
