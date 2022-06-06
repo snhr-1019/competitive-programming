119d7e0868920ede0300aec022efa58011ec37ea
Hiroaki Sone <sonehiro@gmail.com>
2022-06-07 02:14:19 +0900

    ABC254 E WA 要見直し

--- AtCoder/abc254/e/main.py
+++ AtCoder/abc254/e/main.py
@@ -1,1 +1,34 @@
+from collections import deque
 
+n, m = map(int, input().split())
+
+edges = [[] for _ in range(n + 1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
+
+
+def solve(x, k):
+    ans = x
+    que = deque()
+    que.append(x)
+    visited = [False] * (n + 1)
+    visited[x] = True
+    while que and k > 0:
+        cur = que.popleft()
+        for nxt in edges[cur]:
+            if visited[nxt]:
+                continue
+            que.append(nxt)
+            visited[nxt] = True
+            ans += nxt
+        k -= 1
+    print(ans)
+
+
+q = int(input())
+for i in range(q):
+    x, k = map(int, input().split())
+    solve(x, k)
