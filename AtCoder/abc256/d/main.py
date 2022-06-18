b6bb8e4ee2fe8b11f37921e297ac6bccb3fd0e82
Hiroaki Sone <sonehiro@gmail.com>
2022-06-18 22:59:32 +0900

    ABC256 D fix

--- AtCoder/abc256/d/main.py
+++ AtCoder/abc256/d/main.py
@@ -1,1 +1,16 @@
+n = int(input())
+S = []
+for _ in range(n):
+    l, r = map(int, input().split())
+    S.append([l, -1])
+    S.append([r, 1])
+S.sort()
 
+cur = 0
+start = 0
+for s in S:
+    if cur == 0 and s[1] < 0:
+        start = s[0]
+    cur -= s[1]
+    if cur == 0:
+        print(start, s[0])
