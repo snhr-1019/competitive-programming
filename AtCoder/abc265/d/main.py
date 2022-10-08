7da9ede2ef44c08a6c8a217cc51b58d6652ddea6
Hiroaki Sone <sonehiro@gmail.com>
2022-10-09 06:23:24 +0900

    ABC265 D fix

--- AtCoder/abc265/d/main.py
+++ AtCoder/abc265/d/main.py
@@ -1,1 +1,14 @@
+n, p, q, r = map(int, input().split())
+a = list(map(int, input().split()))
 
+acc = [0] * (n + 1)
+acc[0] = 0
+for i in range(n):
+    acc[i + 1] = acc[i] + a[i]
+acc_s = set(acc)
+
+for i in range(n):
+    if p + acc[i] in acc_s and p + q + acc[i] in acc_s and p + q + r + acc[i] in acc_s:
+        print("Yes")
+        exit()
+print("No")
