7aa60a58632f5db0f67b2462038386ff4f421ba8
Hiroaki Sone <sonehiro@gmail.com>
2022-10-14 23:31:24 +0900

    典型90問

--- AtCoder/typical90/006/main.py
+++ AtCoder/typical90/006/main.py
@@ -1,1 +1,21 @@
+n, k = map(int, input().split())
+S = input()
 
+pos = [[-1] * n for _ in range(26)]
+for c in range(26):
+    cur = -1
+    for i in range(n - 1, -1, -1):
+        if ord(S[i]) - ord('a') == c:
+            cur = i
+        pos[c][i] = cur
+
+ans = []
+cur = 0
+while k > 0:
+    for i in range(26):
+        if pos[i][cur] != -1 and 0 < k <= n - pos[i][cur]:
+            ans.append(chr(i + ord('a')))
+            k -= 1
+            cur = pos[i][cur] + 1
+            break
+print(*ans, sep='')
