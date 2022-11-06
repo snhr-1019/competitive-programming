3236b65855d4a2d041f50916073bd484445261d0
Hiroaki Sone <sonehiro@gmail.com>
2022-11-06 15:29:56 +0900

    ABC276 E fix

--- AtCoder/abc276/e/main.py
+++ AtCoder/abc276/e/main.py
@@ -1,10 +1,9 @@
-import sys
-
-sys.setrecursionlimit(10 ** 6)
+from collections import deque
 
 h, w = map(int, input().split())
+
 grid = [list(input()) for _ in range(h)]
-visited = [[False] * w for _ in range(h)]
+cnt = [[0] * w for _ in range(h)]
 for i in range(h):
     for j in range(w):
         if grid[i][j] == 'S':
@@ -15,31 +14,39 @@ for i in range(h):
         continue
     break
 
-
-def dfs(x, y, l):
-    if x < 0 or x >= h or y < 0 or y >= w:
-        return
-
-    # 壁か既に通ったマスならNG
-    if grid[x][y] == '#' or visited[x][y]:
-        return
-
-    if l < 4 and grid[x][y] == 'S':
-        return
-
-    if l >= 4 and grid[x][y] == 'S':
-        print("Yes")
-        exit()
-    visited[x][y] = True
-    dfs(x + 1, y, l + 1)
-    dfs(x - 1, y, l + 1)
-    dfs(x, y + 1, l + 1)
-    dfs(x, y - 1, l + 1)
-    visited[x][y] = False
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 
-dfs(x + 1, y, 1)
-dfs(x - 1, y, 1)
-dfs(x, y + 1, 1)
-dfs(x, y - 1, 1)
+def bfs(sx, sy):
+    visited = [[False] * w for _ in range(h)]
+    que = deque()
+    que.append((sx, sy))
+
+    while que:
+        x, y = que.popleft()
+        # 範囲外
+        if x < 0 or x >= h or y < 0 or y >= w:
+            continue
+
+        # 壁か既に通ったマスならNG
+        if grid[x][y] in ('#', 'S') or visited[x][y]:
+            continue
+        cnt[x][y] += 1
+        visited[x][y] = True
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            que.append((nx, ny))
+
+
+for i in range(4):
+    bfs(x + dx[i], y + dy[i])
+
+for i in range(h):
+    for j in range(w):
+        if cnt[i][j] >= 2:
+            print("Yes")
+            exit()
 print("No")
