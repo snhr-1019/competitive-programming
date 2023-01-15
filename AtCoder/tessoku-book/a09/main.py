e478288f59e548a83add97014a3d8d10828ea388
SONE Hiroaki <sonehiro@gmail.com>
2023-01-15 13:23:23 +0900

    鉄則本 2.4まで

--- AtCoder/tessoku-book/a09/main.py
+++ AtCoder/tessoku-book/a09/main.py
@@ -1,1 +1,22 @@
+h, w, n = map(int, input().split())
+grid = [[0] * (w + 10) for _ in range(h + 10)]
 
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    grid[a][b] += 1
+    grid[a][d + 1] -= 1
+    grid[c + 1][b] -= 1
+    grid[c + 1][d + 1] += 1
+
+acc = [[0] * (w + 10) for _ in range(h + 10)]
+
+for i in range(1, h + 1):
+    for j in range(1, w + 1):
+        acc[i][j] = acc[i][j - 1] + grid[i][j]
+
+for j in range(1, w + 1):
+    for i in range(1, h + 1):
+        acc[i][j] += acc[i - 1][j]
+
+for i in range(1, h + 1):
+    print(*acc[i][1:w + 1])
--- AtCoder/tessoku-book/b09/main.py
+++ AtCoder/tessoku-book/b09/main.py
@@ -1,1 +1,23 @@
+n = int(input())
+grid = [[0] * 1510 for _ in range(1510)]
 
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    grid[a][b] += 1
+    grid[a][d] -= 1
+    grid[c][b] -= 1
+    grid[c][d] += 1
+
+for i in range(1501):
+    for j in range(1, 1501):
+        grid[i][j] += grid[i][j - 1]
+
+for j in range(1501):
+    for i in range(1, 1501):
+        grid[i][j] += grid[i - 1][j]
+
+ans = 0
+for i in range(1501):
+    ans += sum(map(lambda x: x > 0, grid[i][:1501]))
+
+print(ans)
