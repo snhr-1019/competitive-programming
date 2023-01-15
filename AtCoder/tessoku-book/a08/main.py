d45755b1c19adf1de9d8d69ee46aa4177a0a734b
SONE Hiroaki <sonehiro@gmail.com>
2023-01-15 12:25:18 +0900

    鉄則本 2.3まで

--- AtCoder/tessoku-book/a08/main.py
+++ AtCoder/tessoku-book/a08/main.py
@@ -1,1 +1,19 @@
+h, w = map(int, input().split())
+x = [[0] * (w + 1)]
+for _ in range(h):
+    x.append([0] + list(map(int, input().split())))
 
+acc = [[0] * (w + 1) for _ in range(h + 1)]
+
+for i in range(1, h + 1):
+    for j in range(1, w + 1):
+        acc[i][j] = acc[i][j - 1] + x[i][j]
+
+for j in range(1, w + 1):
+    for i in range(1, h + 1):
+        acc[i][j] = acc[i - 1][j] + acc[i][j]
+
+q = int(input())
+for _ in range(q):
+    a, b, c, d = map(int, input().split())
+    print(acc[c][d] + acc[a - 1][b - 1] - acc[a - 1][d] - acc[c][b - 1])
--- AtCoder/tessoku-book/b08/main.py
+++ AtCoder/tessoku-book/b08/main.py
@@ -1,1 +1,22 @@
+n = int(input())
+T = 1501
+p = [[0] * T for _ in range(T)]
 
+for _ in range(n):
+    x, y = map(int, input().split())
+    p[x][y] += 1
+
+acc = [[0] * T for _ in range(T)]
+
+for i in range(1, T):
+    for j in range(1, T):
+        acc[i][j] = acc[i][j - 1] + p[i][j]
+
+for j in range(1, T):
+    for i in range(1, T):
+        acc[i][j] += acc[i - 1][j]
+
+q = int(input())
+for _ in range(q):
+    a, b, c, d = map(int, input().split())
+    print(acc[c][d] + acc[a - 1][b - 1] - acc[a - 1][d] - acc[c][b - 1])
