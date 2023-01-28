6026ec46e50e7199f45614da09b61a25fd8c21fb
SONE Hiroaki <sonehiro@gmail.com>
2023-01-29 00:05:20 +0900

    ABC287 D fix

--- AtCoder/abc287/d/main.py
+++ AtCoder/abc287/d/main.py
@@ -1,1 +1,32 @@
+s = input()
+t = input()
+len_t = len(t)
 
+
+# s, tの長さはともにx文字
+def match(a, b):
+    return a == b or a == '?' or b == '?'
+
+
+pre = [False] * (len_t + 1)
+pre[0] = True
+for x in range(len_t):
+    if not match(s[x], t[x]):
+        break
+    pre[x + 1] = True
+
+s = s[::-1]
+t = t[::-1]
+
+suf = [False] * (len_t + 1)
+suf[0] = True
+for x in range(len_t):
+    if not match(s[x], t[x]):
+        break
+    suf[x + 1] = True
+
+for x in range(len_t + 1):
+    if pre[x] and suf[len_t - x]:
+        print("Yes")
+    else:
+        print("No")
