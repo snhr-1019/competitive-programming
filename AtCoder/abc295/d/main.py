559122a687769909c982cb0eb39bb082c0570fdc
SONE Hiroaki <sonehiro@gmail.com>
2023-03-26 00:38:23 +0900

    ABC295 D fix again

--- AtCoder/abc295/d/main.py
+++ AtCoder/abc295/d/main.py
@@ -2,18 +2,14 @@ from collections import defaultdict
 
 s = input()
 n = len(s)
-acc = [[0] * 10 for _ in range(n + 1)]
+acc = [0] * (n + 1)
 for i in range(n):
     c = int(s[i])
-    acc[i + 1] = acc[i][:]
-    acc[i + 1][c] ^= 1
+    acc[i + 1] = acc[i] ^ (1 << c)
 
 cnt = defaultdict(int)
 for acc_i in acc:
-    b = 0
-    for i in range(10):
-        b |= acc_i[i] << i
-    cnt[b] += 1
+    cnt[acc_i] += 1
 
 ans = 0
 for v in cnt.values():
