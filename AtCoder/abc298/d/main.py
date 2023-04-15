ae14a523bf0abfbb0efd7102ac579f1e9ef38433
SONE Hiroaki <sonehiro@gmail.com>
2023-04-15 22:51:41 +0900

    ABC298 D fix

--- AtCoder/abc298/d/main.py
+++ AtCoder/abc298/d/main.py
@@ -1,18 +1,87 @@
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x - self.x) if isinstance(other, ModInt) else
+            ModInt(other - self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, MOD - 2, MOD))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, MOD))
+        )
+
+
+############
+
+
 from collections import deque
 
-M = 998244353
+MOD = 998244353
 q = int(input())
 
 que = deque()
 que.append("1")
-val = 1
+val = ModInt(1)
 for _ in range(q):
     query = input().split()
     if query[0] == '1':
         que.append(query[1])
-        val = ((val * 10) % M + int(query[1])) % M
+        val = (val * 10) + int(query[1])
     elif query[0] == '2':
         s = que.popleft()
-        val = (val - int(s) * pow(10, len(que), M)) % M
+        val = val - int(s) * pow(10, len(que), MOD)
     elif query[0] == '3':
         print(val)
