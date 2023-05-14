251a755c6555843e07a86c5fd5b60b46dd8640f2
SONE Hiroaki <sonehiro@gmail.com>
2023-05-14 21:58:16 +0900

    bit dp

--- AtCoder/tessoku-book/a23/main.py
+++ AtCoder/tessoku-book/a23/main.py
@@ -1,1 +1,30 @@
+n, m = map(int, input().split())
 
+a = [0]  # 1-indexedに調整
+for _ in range(m):
+    a.append(int(input().replace(" ", ""), 2))
+
+'''
+dp[i][j]
+i番目までのクーポンを使用するか否かを決めたときに、
+j（買える商品をビットで表したもの）を買うために最小のクーポン枚数
+'''
+dp = [[1000] * (1 << n) for _ in range(m + 1)]
+
+dp[0][0] = 0
+
+# i番目のクーポンの使用の有無を決める
+for i in range(1, m + 1):
+    # j: 1つ前のクーポンまでの使用によって買える商品のビット表現
+    for j in range(1 << n):
+        # i番目のクーポンを使用しない場合
+        dp[i][j] = min(dp[i][j], dp[i - 1][j])
+
+        # i番目のクーポンを使用する場合
+        v = j | a[i]
+        dp[i][v] = min(dp[i][v], dp[i - 1][j] + 1)
+
+if dp[m][(1 << n) - 1] == 1000:
+    print(-1)
+else:
+    print(dp[m][(1 << n) - 1])
--- AtCoder/tessoku-book/b23/main.py
+++ AtCoder/tessoku-book/b23/main.py
@@ -1,1 +1,33 @@
+INF = 10 ** 9
+n = int(input())
+xy = [list(map(int, input().split())) for _ in range(n)]
 
+dp = [[INF] * (n + 1) for _ in range(1 << n)]
+
+'''
+dp[i][j]
+i: 既に通った都市をビットで表したものの10進数表現
+j: 今いる都市の番号
+のときの最小コスト
+'''
+dp[0][0] = 0
+
+
+def euclid_dist(i, j):
+    return ((xy[i][0] - xy[j][0]) ** 2 + (xy[i][1] - xy[j][1]) ** 2) ** 0.5
+
+
+for i in range(1 << n):
+    # いま都市jにいる
+    for j in range(n):
+        if dp[i][j] == INF:
+            continue
+        # 次、都市kに向かう
+        for k in range(n):
+            # 既に通った都市はスキップ
+            if i >> k & 1:
+                continue
+
+            v = i | 1 << k
+            dp[v][k] = min(dp[v][k], dp[i][j] + euclid_dist(j, k))
+print(dp[(1 << n) - 1][0])
