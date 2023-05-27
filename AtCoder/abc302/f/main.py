6af991a8292c5ca17bec249fe1fecd6d9787fc24
SONE Hiroaki <sonehiro@gmail.com>
2023-05-27 18:57:37 +0900

    ABC302 F fix

--- AtCoder/abc302/f/main.py
+++ AtCoder/abc302/f/main.py
@@ -1,47 +1,32 @@
 from collections import deque
 
 n, m = map(int, input().split())
-
-s = []
-starts = set()
-goals = set()
-
-t = [set() for _ in range(m + 1)]
-
+edges = [[] for _ in range(n + m)]
 for i in range(n):
-    a = int(input())
-    si = set(map(int, input().split()))
-    s.append(si)
-    if 1 in si:
-        starts.add(i)
-    if m in si:
-        goals.add(i)
+    ai = int(input())
+    si = list(map(int, input().split()))
+    for s in si:
+        s -= 1
+        edges[s].append(m + i)
+        edges[m + i].append(s)
 
-    for sij in si:
-        t[sij].add(i)
+INF = 10 ** 9
+que = deque()
+que.append(0)
+dist = [INF] * (n + m)
+dist[0] = 0
 
-edges = [set() for _ in range(n)]
+while que:
+    cur = que.popleft()
+    for nxt in edges[cur]:
+        if dist[nxt] != INF:
+            continue
 
-for i in range(n):
-    for sij in s[i]:
-        for k in t[sij]:
-            if i == k:
-                continue
-            edges[i].add(k)
-            edges[k].add(i)
+        dist[nxt] = dist[cur] + 1
+        que.append(nxt)
 
-ans = 10 ** 9
-for st in starts:
-    que = deque()
-    que.append((st, 0))
-    while que:
-        cur = que.popleft()
-        if cur[0] in goals:
-            ans = min(ans, cur[1])
-            break
-        for sij in edges[cur[0]]:
-            que.append((sij, cur[1] + 1))
-if ans == 10 ** 9:
+ans = dist[m - 1]
+if ans == INF:
     print(-1)
 else:
-    print(ans)
+    print((ans - 2) // 2)
