2dd6d53d66956476413750d035ff4a5a288f23c0
SONE Hiroaki <sonehiro@gmail.com>
2023-06-04 14:34:21 +0900

    ABC304 D fix

--- AtCoder/abc304/d/main.py
+++ AtCoder/abc304/d/main.py
@@ -1,34 +1,34 @@
+from collections import defaultdict
+from bisect import bisect
+
 w, h = map(int, input().split())
 n = int(input())
-p = []
-q = []
-cake = [[0] * (w + 1) for _ in range(h + 1)]
+cnt = defaultdict(int)
+
+pq = []
+
 for bi in range(n):
     pi, qi = map(int, input().split())
-    cake[qi][pi] = 1
-
-# 2次元累積和を取る
-# 横
-for y in range(h + 1):
-    for x in range(1, w + 1):
-        cake[y][x] += cake[y][x - 1]
-
-# 縦
-for x in range(w + 1):
-    for y in range(1, h + 1):
-        cake[y][x] += cake[y - 1][x]
+    pq.append((pi, qi))
 
 A = int(input())
-a = [0] + list(map(int, input().split())) + [w]
+a = [0] + list(map(int, input().split()))
 B = int(input())
-b = [0] + list(map(int, input().split())) + [h]
+b = [0] + list(map(int, input().split()))
+
+BIAS = 10 ** 6
+for pi, qi in pq:
+    ai = bisect(a, pi) - 1
+    bi = bisect(b, qi) - 1
+    cnt[bi * BIAS + ai] += 1
 
 mn = 10 ** 10
 mx = -1
 
-for ai in range(1, A + 2):
-    for bi in range(1, B + 2):
-        cnt = cake[b[bi]][a[ai]] - cake[b[bi - 1]][a[ai]] - cake[b[bi]][a[ai - 1]] + cake[b[bi - 1]][a[ai - 1]]
-        mn = min(mn, cnt)
-        mx = max(mx, cnt)
+if len(cnt.keys()) < (A + 1) * (B + 1):
+    mn = 0
+
+mn = min(mn, min(cnt.values()))
+mx = max(cnt.values())
+
 print(mn, mx)
