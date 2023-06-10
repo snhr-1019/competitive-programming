754c34d322b84cceefd4eb59d8bdbce8b31cead4
SONE Hiroaki <sonehiro@gmail.com>
2023-06-10 23:25:47 +0900

    ABC305 E fix

--- AtCoder/abc305/e/main.py
+++ AtCoder/abc305/e/main.py
@@ -1,4 +1,4 @@
-from collections import deque
+from heapq import heappush, heappop
 
 n, m, k = map(int, input().split())
 
@@ -10,25 +10,32 @@ for _ in range(m):
     edges[a].append(b)
     edges[b].append(a)
 
-dp = [-1] * n
-que = deque()
+visited = [-1] * n
+hq = []
+
 for _ in range(k):
     p, h = map(int, input().split())
     p -= 1
-    dp[p] = h
-    que.append((p, h))
+    heappush(hq, (-h, p))
+    visited[p] = h
 
-while que:
-    cur, h = que.popleft()
-    h -= 1
+while hq:
+    h, cur = heappop(hq)
+    h = -h
+
+    if visited[cur] != -1 and visited[cur] > h:
+        continue
+    if h == 0:
+        continue
+
     for nxt in edges[cur]:
-        if dp[nxt] < h:
-            dp[nxt] = h
-            que.append((nxt, h))
+        if visited[nxt] == -1 or visited[nxt] < h - 1:
+            visited[nxt] = h - 1
+            heappush(hq, (-(h - 1), nxt))
 
 ans = []
 for i in range(n):
-    if dp[i] >= 0:
+    if visited[i] >= 0:
         ans.append(i + 1)
 print(len(ans))
 print(*ans)
