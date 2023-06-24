67ac163e85d80501da705294e6612ba086933d7e
SONE Hiroaki <sonehiro@gmail.com>
2023-06-24 23:46:32 +0900

    ABC307 C fix

--- AtCoder/abc307/c/main.py
+++ AtCoder/abc307/c/main.py
@@ -1,40 +1,32 @@
 ha, wa = map(int, input().split())
 a = [list(input()) for _ in range(ha)]
-a_black = []
-for i in range(ha):
-    for j in range(wa):
-        if a[i][j] == "#":
-            a_black.append((i, j))
 
 hb, wb = map(int, input().split())
-
 b = [list(input()) for _ in range(hb)]
-b_black = []
-for i in range(hb):
-    for j in range(wb):
-        if b[i][j] == "#":
-            b_black.append((i, j))
 
 hx, wx = map(int, input().split())
 x = [list(input()) for _ in range(hx)]
-x_black = set()
-for i in range(hx):
-    for j in range(wx):
-        if x[i][j] == "#":
-            x_black.add((i, j))
 
-for xb in x_black:
-    black = set()
 
-    for ab in a_black:
-        black.add((ab[0] + xb[0], ab[1] + xb[1]))
+def toSet(t):
+    s = set()
+    for i in range(len(t)):
+        for j in range(len(t[0])):
+            if t[i][j] == "#":
+                s.add((i, j))
+    return s
 
-    for xb2 in x_black:
-        for bb in b_black:
-            black.add((bb[0] + xb2[0], bb[1] + xb2[1]))
 
-        if black == x_black:
-            print("Yes")
-            exit()
+a_set = toSet(a)
+b_set = toSet(b)
+x_set = toSet(x)
 
+for i in range(-10, 11):
+    for j in range(-10, 11):
+        for k in range(-10, 11):
+            for l in range(-10, 11):
+                s = {(ai + i, aj + j) for (ai, aj) in a_set} | {(bi + k, bj + l) for (bi, bj) in b_set}
+                if s == x_set:
+                    print("Yes")
+                    exit()
 print("No")
