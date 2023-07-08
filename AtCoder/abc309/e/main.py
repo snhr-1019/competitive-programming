5a2c9d29789ab8d4f0381ddc31ea1080d71c8321
SONE Hiroaki <sonehiro@gmail.com>
2023-07-08 23:05:52 +0900

    ABC309 E fix

--- AtCoder/abc309/e/main.py
+++ AtCoder/abc309/e/main.py
@@ -1,39 +1,22 @@
-from collections import deque
-from bisect import bisect_right
-
 n, m = map(int, input().split())
-c = [[] for _ in range(n)]
 
-p = list(map(int, input().split()))
-for i in range(n - 1):
-    c[p[i] - 1].append(i + 1)
+p = [0] + list(map(int, input().split()))
+for i in range(n):
+    p[i] -= 1
+
+dp = [-1] * n
 
 x = [0] * m
 y = [0] * m
 for i in range(m):
     xi, yi = map(int, input().split())
     xi -= 1
-    x[i] = xi
-    y[i] = yi
+    dp[xi] = max(dp[xi], yi)
 
-d = [-1] * n
-d[0] = 0
-
-que = deque([0])
-
-while que:
-    cur = que.popleft()
-    for nxt in c[cur]:
-        d[nxt] = d[cur] + 1
-        que.append(nxt)
+for i in range(1, n):
+    dp[i] = max(dp[i], dp[p[i]] - 1)
 
 ans = 0
 for i in range(n):
-    t = bisect_right(x, i)
-    for j in range(t - 1, -1, -1):
-        ins = x[j]
-        if d[ins] - d[i] <= y[j]:
-            print(i, ins)
-            ans += 1
-            break
+    ans += (dp[i] >= 0)
 print(ans)
