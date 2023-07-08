b409d87c0c3f40d7c55cb9cce7e9fa1d4907a8a2
SONE Hiroaki <sonehiro@gmail.com>
2023-07-09 00:15:05 +0900

    ABC309 A fix

--- AtCoder/abc309/a/main.py
+++ AtCoder/abc309/a/main.py
@@ -1,18 +1,6 @@
 a, b = map(int, input().split())
 
-t = {
-    1: [2],
-    2: [1, 3],
-    3: [2],
-    4: [5],
-    5: [4, 6],
-    6: [5],
-    7: [8],
-    8: [7, 9],
-    9: [8],
-}
-
-if b in t[a]:
+if b == a + 1 and a % 3 != 0:
     print('Yes')
 else:
     print('No')
