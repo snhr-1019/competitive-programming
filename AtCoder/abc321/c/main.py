02cae0be9395b0776a3445a007fe71c0a7478ac4
SONE Hiroaki <sonehiro@gmail.com>
2023-09-23 22:53:19 +0900

    ABC321 C fix

--- AtCoder/abc321/c/main.py
+++ AtCoder/abc321/c/main.py
@@ -1,1 +1,15 @@
-
+k = int(input())
+cand = []
+for i in range(2 ** 10):
+    s = []
+    for j in range(10):
+        if i >> j & 1:
+            s.append(j)
+    if len(s) == 0:
+        continue
+    if len(s) == 1 and s == 0:
+        continue
+    s.reverse()
+    cand.append(int(''.join(map(str, s))))
+cand.sort()
+print(cand[k])
